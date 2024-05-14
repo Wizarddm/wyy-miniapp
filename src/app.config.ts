76f09f566ee5050ae2100d947c2642617a1482b9
Wizarddm <908877630@qq.com>
2024-05-14 16:09:04 +0800

    1. 限时优惠页面ui

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,9 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index", "pages/limited-time-discount/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
